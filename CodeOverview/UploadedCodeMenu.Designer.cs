07dd5489daddf910704a0d82ae17e49759e4f04e
thuytrang-hoang <thuytrang.hoangt@gmail.com>
2026-04-15 20:43:49 +0200

    Commit Code Overview Feature

---
+++ CodeOverview/UploadedCodeMenu.Designer.cs
@@ -0,0 +1,262 @@
+﻿namespace Programmierprojekt.Managers_Menus.FilesOfCode
+{
+    partial class UploadedCodeMenu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            filesListViewBox = new ListView();
+            uploadedFromUser = new ColumnHeader();
+            uploadedFile = new ColumnHeader();
+            fileSize = new ColumnHeader();
+            uploadedDate = new ColumnHeader();
+            uploadFileToListViewBox = new Button();
+            downloadFileToDesktop = new Button();
+            removeFile = new Button();
+            topBarControl1 = new Topbar.TopBarControl();
+            sidebar1 = new Sidebar();
+            title = new Label();
+            lblCodeOverview = new Label();
+            lblProjekte = new Label();
+            lblHomepage = new Label();
+            lblTrennung1 = new Label();
+            lblTrennung2 = new Label();
+            SuspendLayout();
+            // 
+            // filesListViewBox
+            // 
+            filesListViewBox.BackColor = Color.White;
+            filesListViewBox.Columns.AddRange(new ColumnHeader[] { uploadedFromUser, uploadedFile, fileSize, uploadedDate });
+            filesListViewBox.FullRowSelect = true;
+            filesListViewBox.Location = new Point(220, 225);
+            filesListViewBox.Name = "filesListViewBox";
+            filesListViewBox.Size = new Size(776, 328);
+            filesListViewBox.TabIndex = 4;
+            filesListViewBox.UseCompatibleStateImageBehavior = false;
+            filesListViewBox.View = View.Details;
+            // 
+            // uploadedFromUser
+            // 
+            uploadedFromUser.Text = "Nutzer";
+            uploadedFromUser.Width = 185;
+            // 
+            // uploadedFile
+            // 
+            uploadedFile.Text = "Datei";
+            uploadedFile.Width = 185;
+            // 
+            // fileSize
+            // 
+            fileSize.Text = "Größe";
+            fileSize.Width = 185;
+            // 
+            // uploadedDate
+            // 
+            uploadedDate.Text = "Hochgeladen";
+            uploadedDate.Width = 200;
+            // 
+            // uploadFileToListViewBox
+            // 
+            uploadFileToListViewBox.BackColor = Color.FromArgb(235, 232, 240);
+            uploadFileToListViewBox.Location = new Point(220, 176);
+            uploadFileToListViewBox.Margin = new Padding(3, 4, 3, 4);
+            uploadFileToListViewBox.Name = "uploadFileToListViewBox";
+            uploadFileToListViewBox.Size = new Size(154, 29);
+            uploadFileToListViewBox.TabIndex = 5;
+            uploadFileToListViewBox.Text = "Upload";
+            uploadFileToListViewBox.UseVisualStyleBackColor = false;
+            uploadFileToListViewBox.Click += uploadFileToListViewBox_Click;
+            // 
+            // downloadFileToDesktop
+            // 
+            downloadFileToDesktop.BackColor = Color.FromArgb(235, 232, 240);
+            downloadFileToDesktop.Location = new Point(398, 176);
+            downloadFileToDesktop.Margin = new Padding(3, 4, 3, 4);
+            downloadFileToDesktop.Name = "downloadFileToDesktop";
+            downloadFileToDesktop.Size = new Size(154, 29);
+            downloadFileToDesktop.TabIndex = 6;
+            downloadFileToDesktop.Text = "Download";
+            downloadFileToDesktop.UseVisualStyleBackColor = false;
+            downloadFileToDesktop.Click += downloadFileToDesktop_Click;
+            // 
+            // removeFile
+            // 
+            removeFile.BackColor = Color.FromArgb(235, 232, 240);
+            removeFile.Location = new Point(842, 176);
+            removeFile.Margin = new Padding(3, 4, 3, 4);
+            removeFile.Name = "removeFile";
+            removeFile.Size = new Size(154, 29);
+            removeFile.TabIndex = 7;
+            removeFile.Text = "Remove";
+            removeFile.UseVisualStyleBackColor = false;
+            removeFile.Click += removeFile_Click;
+            // 
+            // topBarControl1
+            // 
+            topBarControl1.Dock = DockStyle.Top;
+            topBarControl1.Location = new Point(0, 0);
+            topBarControl1.Name = "topBarControl1";
+            topBarControl1.Size = new Size(1008, 60);
+            topBarControl1.TabIndex = 8;
+            // 
+            // sidebar1
+            // 
+            sidebar1.BackColor = Color.LavenderBlush;
+            sidebar1.Dock = DockStyle.Left;
+            sidebar1.Location = new Point(0, 60);
+            sidebar1.Name = "sidebar1";
+            sidebar1.Size = new Size(204, 525);
+            sidebar1.TabIndex = 9;
+            // 
+            // title
+            // 
+            title.AutoSize = true;
+            title.BackColor = Color.Transparent;
+            title.Font = new Font("Segoe UI", 20F);
+            title.ForeColor = Color.FromArgb(64, 0, 64);
+            title.Location = new Point(220, 117);
+            title.Name = "title";
+            title.Size = new Size(255, 46);
+            title.TabIndex = 10;
+            title.Text = "Code-Übersicht";
+            // 
+            // lblCodeOverview
+            // 
+            lblCodeOverview.AutoSize = true;
+            lblCodeOverview.Cursor = Cursors.Hand;
+            lblCodeOverview.Font = new Font("Segoe UI", 10.2F, FontStyle.Italic, GraphicsUnit.Point, 0);
+            lblCodeOverview.ForeColor = SystemColors.ControlText;
+            lblCodeOverview.Location = new Point(387, 63);
+            lblCodeOverview.Name = "lblCodeOverview";
+            lblCodeOverview.Size = new Size(122, 23);
+            lblCodeOverview.TabIndex = 24;
+            lblCodeOverview.Text = "Code-Übersicht";
+            lblCodeOverview.MouseEnter += lblCodeOverview_MouseEnter;
+            lblCodeOverview.MouseLeave += lblCodeOverview_MouseLeave;
+            // 
+            // lblProjekte
+            // 
+            lblProjekte.AutoSize = true;
+            lblProjekte.BackColor = Color.Transparent;
+            lblProjekte.Cursor = Cursors.Hand;
+            lblProjekte.FlatStyle = FlatStyle.Flat;
+            lblProjekte.Font = new Font("Segoe UI", 10.2F, FontStyle.Italic, GraphicsUnit.Point, 0);
+            lblProjekte.ForeColor = SystemColors.WindowFrame;
+            lblProjekte.Location = new Point(308, 63);
+            lblProjekte.Name = "lblProjekte";
+            lblProjekte.Size = new Size(69, 23);
+            lblProjekte.TabIndex = 22;
+            lblProjekte.Text = "Projekte";
+            lblProjekte.Click += lblProjekte_Click;
+            lblProjekte.MouseEnter += lblProjekte_MouseEnter;
+            lblProjekte.MouseLeave += lblProjekte_MouseLeave;
+            // 
+            // lblHomepage
+            // 
+            lblHomepage.AutoSize = true;
+            lblHomepage.BackColor = Color.Transparent;
+            lblHomepage.Cursor = Cursors.Hand;
+            lblHomepage.FlatStyle = FlatStyle.Flat;
+            lblHomepage.Font = new Font("Segoe UI", 10.2F, FontStyle.Italic, GraphicsUnit.Point, 0);
+            lblHomepage.ForeColor = SystemColors.WindowFrame;
+            lblHomepage.Location = new Point(210, 63);
+            lblHomepage.Name = "lblHomepage";
+            lblHomepage.Size = new Size(89, 23);
+            lblHomepage.TabIndex = 20;
+            lblHomepage.Text = "Homepage";
+            lblHomepage.Click += lblHomepage_Click;
+            lblHomepage.MouseEnter += lblHomepage_MouseEnter;
+            lblHomepage.MouseLeave += lblHomepage_MouseLeave;
+            // 
+            // lblTrennung1
+            // 
+            lblTrennung1.AutoSize = true;
+            lblTrennung1.Font = new Font("Segoe UI", 10.2F, FontStyle.Italic, GraphicsUnit.Point, 0);
+            lblTrennung1.ForeColor = SystemColors.WindowFrame;
+            lblTrennung1.Location = new Point(296, 63);
+            lblTrennung1.Name = "lblTrennung1";
+            lblTrennung1.Size = new Size(17, 23);
+            lblTrennung1.TabIndex = 21;
+            lblTrennung1.Text = "/";
+            // 
+            // lblTrennung2
+            // 
+            lblTrennung2.AutoSize = true;
+            lblTrennung2.BackColor = Color.Transparent;
+            lblTrennung2.Font = new Font("Segoe UI", 10.2F, FontStyle.Italic, GraphicsUnit.Point, 0);
+            lblTrennung2.ForeColor = SystemColors.WindowFrame;
+            lblTrennung2.Location = new Point(374, 63);
+            lblTrennung2.Name = "lblTrennung2";
+            lblTrennung2.Size = new Size(17, 23);
+            lblTrennung2.TabIndex = 23;
+            lblTrennung2.Text = "/";
+            // 
+            // UploadedCodeMenu
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.White;
+            ClientSize = new Size(1008, 585);
+            Controls.Add(lblCodeOverview);
+            Controls.Add(lblProjekte);
+            Controls.Add(lblHomepage);
+            Controls.Add(lblTrennung1);
+            Controls.Add(lblTrennung2);
+            Controls.Add(title);
+            Controls.Add(sidebar1);
+            Controls.Add(topBarControl1);
+            Controls.Add(removeFile);
+            Controls.Add(downloadFileToDesktop);
+            Controls.Add(uploadFileToListViewBox);
+            Controls.Add(filesListViewBox);
+            Margin = new Padding(3, 4, 3, 4);
+            MaximizeBox = false;
+            Name = "UploadedCodeMenu";
+            Text = "UploadedCodeMenu";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private ListView filesListViewBox;
+        private ColumnHeader uploadedFromUser;
+        private ColumnHeader uploadedFile;
+        private ColumnHeader fileSize;
+        private ColumnHeader uploadedDate;
+        private Button uploadFileToListViewBox;
+        private Button downloadFileToDesktop;
+        private Button removeFile;
+        private Topbar.TopBarControl topBarControl1;
+        private Sidebar sidebar1;
+        private Label title;
+        private Label lblCodeOverview;
+        private Label lblProjekte;
+        private Label lblHomepage;
+        private Label lblTrennung1;
+        private Label lblTrennung2;
+    }
+}
---
+++ CodeOverview/UploadedCodeMenu.cs
@@ -0,0 +1,310 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Extensions.Configuration;
+using System.Data.SqlClient;
+using Programmierprojekt.Datenbank;
+using System.Numerics;
+using Microsoft.VisualBasic.ApplicationServices;
+using System.Reflection;
+using Programmierprojekt.Homepage;
+
+namespace Programmierprojekt.Managers_Menus.FilesOfCode
+{
+    public partial class UploadedCodeMenu : Form //Assignee: Ugur Bektas; Developer of GUI and  functionality: Ugur Bektas; Translator: Languagemodel-Chat-GPT
+    {
+        private readonly IConfiguration Configuration;
+
+        public UploadedCodeMenu()
+        {
+            InitializeComponent();
+
+            this.StartPosition = FormStartPosition.CenterScreen; // Center window
+            var builder = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+            Configuration = builder.Build();
+            this.Activated += (s, e) => LoadUploadedFiles();
+        }
+        /* Helper: Chat-GPT Prompt: "Wie lade ich eine Datei hoch"
+         * Based on helped Output i adjusted it 
+         */
+        private void LoadUploadedFiles()
+        {
+            string connectionString = Configuration.GetConnectionString("DefaultConnection");
+            filesListViewBox.Items.Clear();
+
+            string query = @"SELECT ucf.fileIDUploadedCodeFiles,  ucf.fileNameUploadedCodeFiles,  ucf.uploadedDate,  u.username AS uploadBy  FROM UploadedCodeFiles ucf INNER JOIN [User] u ON ucf.userId = u.userId INNER JOIN UserGroups ug ON u.userId = ug.userId WHERE ug.groupId IN (SELECT groupId FROM UserGroups WHERE userId = @userId);";
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    conn.Open();
+                    using (SqlCommand cmd = new SqlCommand(query, conn))
+                    {
+                        cmd.Parameters.AddWithValue("@userId", UserSession.Instance.UserId);
+
+                        using (SqlDataReader reader = cmd.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                string fileId = reader["fileIDUploadedCodeFiles"].ToString();
+                                string fileName = reader["fileNameUploadedCodeFiles"].ToString();
+                                string uploadDate = Convert.ToDateTime(reader["uploadedDate"]).ToString("dd/MM/yyyy HH:mm");
+                                string uploadedBy = reader["uploadBy"].ToString();
+
+                                ListViewItem item = new ListViewItem(uploadedBy);
+                                item.SubItems.Add(fileName);
+                                item.SubItems.Add("N/A"); //Later File Size
+                                item.SubItems.Add(uploadDate);
+                                item.Tag = fileId;
+
+                                filesListViewBox.Items.Add(item);
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Fehler beim Laden der Dateien: {ex.Message}", "Fehler", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        //Code from CHAT-GPT; Prompt how to convert into bigint,
+        /*  private string FormatFileSize(long bytes)
+          {
+              if (bytes < 1024) return $"{bytes} Bytes";
+              if (bytes < 1024 * 1024) return $"{bytes / 1024.0:F2} KB";
+              if (bytes < 1024 * 1024 * 1024) return $"{bytes / (1024.0 * 1024.0):F2} MB";
+              return $"{bytes / (1024.0 * 1024.0 * 1024.0):F2} GB";
+          }*/
+
+        private void uploadFileToListViewBox_Click(object sender, EventArgs e)
+        {
+            string connectionString = Configuration.GetConnectionString("DefaultConnection");
+
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+                openFileDialog.Filter = "Alle Dateien (*.*)|*.*";
+                openFileDialog.Title = "Datei zum Hochladen auswählen";
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    string filePath = openFileDialog.FileName;
+                    string fileName = Path.GetFileName(filePath);
+                    byte[] fileData = File.ReadAllBytes(filePath);
+                    /*   FileInfo fileInfo = new FileInfo(filePath);
+                         long fileSize = fileInfo.Length;*/
+                    int userId = UserSession.Instance.UserId;
+
+                    string query = "INSERT INTO [UploadedCodeFiles] (fileNameUploadedCodeFiles, fileDataUploadedCodeFiles, userId, uploadedDate) VALUES (@fileName, @fileData, @userId, @uploadedDate)";
+
+
+                    try
+                    {
+                        using (SqlConnection conn = new SqlConnection(connectionString))
+                        {
+                            conn.Open();
+                            using (SqlCommand cmd = new SqlCommand(query, conn))
+                            {
+                                cmd.Parameters.AddWithValue("@fileName", fileName);
+                                cmd.Parameters.AddWithValue("@fileData", fileData);
+                                cmd.Parameters.AddWithValue("@uploadedDate", DateTime.Now);
+                                cmd.Parameters.AddWithValue("@userId", userId);
+                                //       cmd.Parameters.AddWithValue("@fileSize", fileSize);
+                                int count = cmd.ExecuteNonQuery();
+                                if (count == 1)
+                                {
+                                    MessageBox.Show("Datei erfolgreich hochgeladen!", "Erfolg", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                                }
+                            }
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show($"Fehler beim Hochladen: {ex.Message}", "Fehler", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+            }
+        }
+
+        private void downloadFileToDesktop_Click(object sender, EventArgs e)
+        {
+            string connectionString = Configuration.GetConnectionString("DefaultConnection");
+            if (filesListViewBox.SelectedItems.Count == 0)
+            {
+                MessageBox.Show("Bitte eine Datei zum Herunterladen auswählen.", "Fehler", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            int fileId = Convert.ToInt32(filesListViewBox.SelectedItems[0].Tag);
+
+            string query = "SELECT fileNameUploadedCodeFiles, fileDataUploadedCodeFiles FROM UploadedCodeFiles WHERE fileIDUploadedCodeFiles = @fileId";
+
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    conn.Open();
+                    using (SqlCommand cmd = new SqlCommand(query, conn))
+                    {
+                        cmd.Parameters.AddWithValue("@fileId", fileId);
+                        using (SqlDataReader reader = cmd.ExecuteReader())
+                        {
+                            if (reader.Read())
+                            {
+                                string fileName = reader.GetString(0);
+                                byte[] fileData = (byte[])reader[1];
+
+                                string savePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), fileName);
+                                File.WriteAllBytes(savePath, fileData);
+
+                                MessageBox.Show($"Datei gespeichert: {savePath}", "Download abgeschlossen", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Fehler beim Herunterladen: {ex.Message}", "Fehler", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+        private void removeFile_Click(object sender, EventArgs e)
+        {
+            string connectionString = Configuration.GetConnectionString("DefaultConnection");
+
+            if (filesListViewBox.SelectedItems.Count == 0)
+            {
+                MessageBox.Show("Bitte eine Datei zum Löschen auswählen.", "Fehler", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            ListViewItem selectedItem = filesListViewBox.SelectedItems[0];
+            int fileId = Convert.ToInt32(filesListViewBox.SelectedItems[0].Tag);
+
+            string query = "DELETE FROM UploadedCodeFiles WHERE fileIDUploadedCodeFiles = @fileId";
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    conn.Open();
+                    using (SqlCommand cmd = new SqlCommand(query, conn))
+                    {
+                        cmd.Parameters.AddWithValue("@fileId", fileId);
+
+                        int rowsAffected = cmd.ExecuteNonQuery();
+                        if (rowsAffected > 0)
+                        {
+                            filesListViewBox.Items.Remove(selectedItem);
+
+                        }
+                        else
+                        {
+                            MessageBox.Show("Datei nicht gefunden!", "Fehler", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Fehler beim Löschen: {ex.Message}", "Fehler", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+        private void lblHomepage_Click(object sender, EventArgs e)
+        {
+            // Close
+            Form currentForm = this.FindForm(); // Ensures where Topbar is located at
+            if (currentForm != null)
+            {
+                currentForm.Hide(); // With Hide, because issue with closing whole program
+
+                // Open
+                if (FormManager.HomepageFormInstance == null || FormManager.HomepageFormInstance.IsDisposed)
+                {
+                    FormManager.HomepageFormInstance = new HomepageForm();
+
+                }  // Checks if an  ProjectMenuInstance is selected and exists then redirects the user appropriately.
+                FormManager.HomepageFormInstance.Show();
+            }
+        }
+
+        private void lblHomepage_MouseEnter(object sender, EventArgs e)
+        {
+            Label stepLabel = sender as Label;
+            if (stepLabel != null)
+            {
+                stepLabel.Font = new Font(stepLabel.Font, FontStyle.Underline | FontStyle.Italic);
+            }
+        }
+
+        private void lblHomepage_MouseLeave(object sender, EventArgs e)
+        {
+            Label stepLabel = sender as Label;
+            if (stepLabel != null)
+            {
+                stepLabel.Font = new Font(stepLabel.Font, FontStyle.Italic);
+            }
+        }
+
+        private void lblProjekte_Click(object sender, EventArgs e)
+        {
+            // Close
+            Form currentForm = this.FindForm(); // Ensures where Topbar is located at
+            if (currentForm != null)
+            {
+                currentForm.Hide(); // With Hide, because issue with closing whole program
+
+                // Open
+                if (FormManager.ProjectMenuInstance == null || FormManager.ProjectMenuInstance.IsDisposed)
+                {
+                    FormManager.ProjectMenuInstance = new ProjectMenu();
+
+                }  // Checks if an  ProjectMenuInstance is selected and exists then redirects the user appropriately.
+                FormManager.ProjectMenuInstance.Show();
+            }
+        }
+
+        private void lblProjekte_MouseEnter(object sender, EventArgs e)
+        {
+            Label stepLabel = sender as Label;
+            if (stepLabel != null)
+            {
+                stepLabel.Font = new Font(stepLabel.Font, FontStyle.Underline | FontStyle.Italic);
+            }
+        }
+
+        private void lblProjekte_MouseLeave(object sender, EventArgs e)
+        {
+            Label stepLabel = sender as Label;
+            if (stepLabel != null)
+            {
+                stepLabel.Font = new Font(stepLabel.Font, FontStyle.Italic);
+            }
+        }
+        private void lblCodeOverview_MouseEnter(object sender, EventArgs e)
+        {
+            Label stepLabel = sender as Label;
+            if (stepLabel != null)
+            {
+                stepLabel.Font = new Font(stepLabel.Font, FontStyle.Underline | FontStyle.Italic);
+            }
+        }
+
+        private void lblCodeOverview_MouseLeave(object sender, EventArgs e)
+        {
+            Label stepLabel = sender as Label;
+            if (stepLabel != null)
+            {
+                stepLabel.Font = new Font(stepLabel.Font, FontStyle.Italic);
+            }
+        }
+    }
+}
